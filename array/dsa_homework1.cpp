dfdd41a41cda4f4a7e54ce861c5f5559259fccf8
khushalshrimal <khushalshrimal@gmail.com>
2026-06-02 16:55:48 +0530

    DSA_ARRAY homework1

---
+++ array/dsa_homework1.cpp
@@ -0,0 +1,17 @@
+//wap to calculate sum and product of all numbers in an array
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4};
+    int size=4;
+    int sum=0;
+    for(int i=0;i<size;i++){
+       sum=sum+arr[i]; 
+
+
+    }
+    cout<<"sum of array"<<endl<<sum<<endl;
+    return 0;
+
+}
