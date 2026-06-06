1d3752ff8b0a5245f17f49384e8cd91e481ec7ce
khushalshrimal <khushalshrimal@gmail.com>
2026-06-06 11:14:54 +0530

    subarray_print

---
+++ array/subarray.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int arr[]={1,2,3,4,5};
+int size=5;
+for(int start=0;start<size;start++){
+    for(int end=start;end<size;end++){
+        for(int i=start;i<=end;i++){
+            cout<<arr[i];
+        }
+        cout<<endl;
+
+        
+    }
+}
+cout<<endl;
+return 0;
+
+}
