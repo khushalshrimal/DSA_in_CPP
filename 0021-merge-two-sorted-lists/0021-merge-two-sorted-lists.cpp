74f89377a41634f99a7cfc5217ff591544ea16e8
khushalshrimal <khushalshrimal@gmail.com>
2026-08-13 12:09:52 +0530

    Time: 0 ms (100%), Space: 19.5 MB (63.46%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,51 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 == NULL)
-            return list2;
 
-        if(list2 == NULL)
-            return list1;
+        ListNode dummy(0);
+        ListNode* current = &dummy;
 
+        while(list1 != NULL && list2 != NULL) {
 
-        if(list2->val < list1->val) {
-            ListNode* temp = list1;
-            list1 = list2;
-            list2 = temp;
+            if(list1->val <= list2->val) {
+                current->next = list1;
+                list1 = list1->next;
+            }
+            else {
+                current->next = list2;
+                list2 = list2->next;
+            }
+
+            current = current->next;
         }
 
-        ListNode* current1=list1;
-        ListNode* current2=list2;
-        while(current1!=NULL && current2!=NULL){
-        if(current1->next == NULL || current1->next->val >= current2->val){
-            ListNode* newnode=current2;
-            ListNode* temp=current1->next;
-            ListNode* next2=current2->next;
-            current1->next=newnode;
-            current1->next->next=temp;
-            current1=current1->next;
-            current2=next2;
+        if(list1 != NULL) {
+            current->next = list1;
+        }
+        else {
+            current->next = list2;
+        }
 
-        }
-        else{
-            current1=current1->next;
-        }
-        
-        }
-        if(current2!=NULL){
-            current1->next=current2;
-        }
-        return list1;
-        
+        return dummy.next;
     }
 };
